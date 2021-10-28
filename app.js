387f3773ba4a9794cce91b4d7eba336bf0d18c46
Ayyoub <mac@Air.local>
2021-10-28 19:34:21 +0100

    Possible fix ..

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
-const btns = document.querySelectorAll('.btn')
-const Audio = document.querySelectorAll('audio')
+const btns = document.querySelectorAll(".btn");
+const Audio = document.querySelectorAll("audio");
 
-window.addEventListener('keydown', (e) => {
-  const audio = document.querySelector(`audio[data-audio ="${e.keyCode}"]`)
-  if (!audio) return
-  audio.play()
+window.addEventListener("keydown", (e) => {
+    const audio = document.querySelector(`audio[data-audio ="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.play();
 
-  const btn = document.querySelector(`button[data-button="${e.keyCode}"]`)
-  btn.classList.add('popup')
-})
+    const btn = document.querySelector(`button[data-button="${e.keyCode}"]`);
+    btn.classList.add("popup");
+});
 
 function removePopup(e) {
-  if (e.propretyName !== 'transform') {
-    this.classList.remove('popup')
-  } else {
-    console.log('wrong')
-  }
+    if (e.propretyName !== "transform") {
+        this.classList.remove("popup");
+    } else {
+        console.log("wrong");
+    }
 }
 
 function clickPopup(e) {
-  if (
-    e.target !== `data-button="window.keyCode"` &&
-    Audio.outerHtml !== `data-audio= "window.keyCode"`
-  ) {
-    this.classList.add('popup')
-    Audio.forEach((aud) => {
-      aud.play()
-    })
-  } else {
-    console.log(audio)
-  }
-  console.log(Audio)
+    if (e.target !== `data-button="window.keyCode"` && Audio.outerHtml !== `data-audio= "window.keyCode"`) {
+        this.classList.add("popup");
+
+        switch (e.target.getAttribute("data-button")) {
+            case "65":
+                document.querySelector(`audio[data-audio ="65"]`).play();
+        }
+    } else {
+        console.log(audio);
+    }
+    console.log(Audio);
 }
 
 btns.forEach((btn) => {
-  btn.addEventListener('transitionend', removePopup)
-  btn.addEventListener('click', clickPopup)
-})
+    btn.addEventListener("transitionend", removePopup);
+    btn.addEventListener("click", clickPopup);
+});
